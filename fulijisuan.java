09a8b4249526dd4e4904ebfe2b49298287e807ce
aa1 <1151484028@qq.com>
2016-03-17 20:46:03 +0800

    03

--- fulijisuan.java
+++ fulijisuan.java
@@ -11,8 +11,7 @@ public class fulijisuan {
 
 		 
 		/*
-		 * 复利计算公式:
-		 * F=P*(1+i)N(次方)
+		 
 　　 F：复利终值
 　　 m：本金
 　　 r：利率
@@ -27,9 +26,9 @@ public class fulijisuan {
 		int choise;
 		Scanner scanner =new Scanner(System.in);
 		do{
-			System.out.println("求复利终值请选择输入1，求本金请输入2：");
+			System.out.println("求复利终值请输入1,求本金请输入2,求利率请输入3，求年限请输入4,求利滚利请输入5：");
 		    choise=scanner.nextInt();
-		}while(choise!=1&&choise!=2);
+		}while(choise!=1&&choise!=2&&choise!=3&&choise!=4&&choise!=5);
 		if(choise==1){
 		
 		Scanner input = new Scanner(System.in);
@@ -65,6 +64,59 @@ public class fulijisuan {
 		double m= F/(Math.pow((1+r/n), y*n));
 		System.out.println("本金:"+df2.format(m));
 	}
+	
+	if(choise==3){
+		Scanner input = new Scanner(System.in);
+		System.out.println("请输入存入复利终值:");
+		double F= input.nextDouble();
+	    System.out.println("请输入本金:");
+	    double  m= input.nextDouble();
+		System.out.println("请输入存入年限:");
+		int y = input.nextInt();
+		System.out.println("请输入年复利次数：");
+		int n = input.nextInt();
+		
+		double r =( Math.pow(F/m, 1.0 / y))-1;
+		System.out.println("利率:"+r);
+	}
+	
+	
+	if(choise==4){
+		Scanner input = new Scanner(System.in);
+		System.out.println("请输入存入复利终值:");
+		double F= input.nextDouble();
+	    System.out.println("请输入年利率:");
+	    double r = input.nextDouble();
+		System.out.println("请输入存入本金:");
+		int m = input.nextInt();
+		System.out.println("请输入年复利次数：");
+		int n = input.nextInt();
+		
+		double y=Math.log((double)(F/m))/(Math.log((double)(1+r)));
+		System.out.println("年限:"+y);
+	}
+	
+	
+	if(choise==5){
+		Scanner input = new Scanner(System.in);
+		System.out.println("请输入存入本金:");
+		double m = input.nextDouble();
+	    System.out.println("请输入年利率:");
+	    double r = input.nextDouble();
+		System.out.println("请输入存入年限:");
+		int y = input.nextInt();
+		System.out.println("请输入年复利次数：");
+		int n = input.nextInt();
+		DecimalFormat df2 = new DecimalFormat("#.000");
+		
+		for (int j = 0; j < y*n; j++) {
+			m = m * (1 + r);
+		}
+		double F = m;
+	
+	System.out.println("利滚利:"+df2.format(F));
+	}
+	
 
 	}
 }
